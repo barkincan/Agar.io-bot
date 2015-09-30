35af86bd65e35b1c9dc055bebc6d19b56e6fe675
barkincan <barkin_buket@hotmail.com>
2015-09-30 15:37:19 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -23,7 +23,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agarz.com/*
 // @version     3.63
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
